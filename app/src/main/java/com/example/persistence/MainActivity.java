f27c49884d14a8650166a6c3de111e3a2be5c4cb
c21alest <alexsanderstrom@outlook.com>
2022-05-10 16:19:12 +0200

    Kopplade variabler mot sina widgets

--- app/src/main/java/com/example/persistence/MainActivity.java
+++ app/src/main/java/com/example/persistence/MainActivity.java
@@ -4,11 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private SQLiteDatabase database;
     private DatabaseHelper databaseHelper;
+
+    private EditText dataField1;
+    private EditText dataField2;
+    private EditText dataField3;
+    private EditText dataField4;
+
+    private Button readField1;
+    private Button writeField1;
+
+    private Button readField2;
+    private Button writeField2;
+
+    private Button readField3;
+    private Button writeField3;
+
+    private Button readField4;
+    private Button writeField4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +36,22 @@ public class MainActivity extends AppCompatActivity {
 
         databaseHelper = new DatabaseHelper(this);
         database = databaseHelper.getWritableDatabase();
+
+        dataField1 = findViewById(R.id.first_field);
+        dataField2 = findViewById(R.id.second_field);
+        dataField3 = findViewById(R.id.third_field);
+        dataField4 = findViewById(R.id.fourth_field);
+
+        readField1 = findViewById(R.id.first_button_read);
+        writeField1 = findViewById(R.id.first_button_write);
+
+        readField2 = findViewById(R.id.second_button_read);
+        writeField2 = findViewById(R.id.second_button_write);
+
+        readField3 = findViewById(R.id.third_button_read);
+        writeField3 = findViewById(R.id.third_button_write);
+
+        readField4 = findViewById(R.id.fourth_button_read);
+        writeField4 = findViewById(R.id.fourth_button_write);
     }
 }
